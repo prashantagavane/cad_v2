58a086be229a902585db4063622d33642ea9829a
prashantagavane <p.agawane7@gmail.com>
2019-12-04 12:56:17 +0200

    1st deploy on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root :to => 'home#index'
+  get 'home/index'
+  #root to: "home#index"
   resources :orders
   resources :rates
   resources :vendors do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_194542) do
+ActiveRecord::Schema.define(version: 2019_12_04_103640) do
 
   create_table "orders", force: :cascade do |t|
     t.string "origin"
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 2019_12_03_194542) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["vendor_id"], name: "index_rates_on_vendor_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vendors", force: :cascade do |t|
